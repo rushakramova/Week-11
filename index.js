c66b014b0d7627ebf8a2b59b412961621fbb927e
rushakramova <rushemz@yandex.ru>
2023-05-21 18:48:37 +0400

    add files

---
+++ index.js
@@ -0,0 +1,16 @@
+const firstPrice = +(document.getElementById('firstPrice').textContent);
+const secondPrice = +(document.getElementById('secondPrice').textContent);
+const thirdPrice = +(document.getElementById('thirdPrice').textContent);
+const fourthPrice = +(document.getElementById('fourthPrice').textContent);
+
+let totalPrice =  document.getElementById('totalPrice');
+
+totalPrice.textContent = (firstPrice + secondPrice + thirdPrice + fourthPrice);
+
+let summary = totalPrice.textContent;
+
+let discountPrice = ((Number(summary))*.8);
+
+const BookSale = () => {
+    totalPrice.textContent = discountPrice.toFixed();
+};
